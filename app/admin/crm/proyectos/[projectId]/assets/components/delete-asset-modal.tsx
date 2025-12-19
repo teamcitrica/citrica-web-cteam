aa87d72b928293fca37d922e59afa26de66ae3bf
YolandaC <yolanda.castrog@outlook.com>
2025-12-19 18:36:35 -0500

    Add estilos segun versión de figma 19 Dic a los modal de eliminación de las opciones del crm

--- app/admin/crm/proyectos/[projectId]/assets/components/delete-asset-modal.tsx
+++ app/admin/crm/proyectos/[projectId]/assets/components/delete-asset-modal.tsx
@@ -6,9 +6,12 @@ import {
   ModalBody,
   ModalFooter,
   Button,
+  Divider,
 } from "@heroui/react";
 
 import { Asset } from "@/hooks/assets/use-assets";
+import { Icon, Text } from "@/shared/components/citrica-ui";
+import { ButtonCitricaAdmin } from "@/shared/components/citrica-ui/admin";
 
 interface DeleteAssetModalProps {
   asset: Asset;
@@ -24,27 +27,41 @@ export default function DeleteAssetModal({
   return (
     <Modal isOpen={true} onClose={onCancel} size="md">
       <ModalContent>
-        <ModalHeader className="flex flex-col gap-1">
-          <h3 className="text-lg font-semibold text-gray-800">
-            Confirmar Eliminación
-          </h3>
+        <ModalHeader className="flex flex-col gap-1 mt-4">
+          <div className="flex items-center justify-center mb-2">
+            <Icon size={28} className=" text-red-500" name="TriangleAlert" />
+          </div>
+          <h2 className="text-center">
+            <Text variant="title" color="#F04242" weight="bold">Confirmar Eliminación</Text>
+          </h2>
         </ModalHeader>
         <ModalBody>
           <p className="text-gray-700">
-            ¿Está seguro de que desea eliminar el asset{" "}
-            <span className="font-semibold">{asset.name || "Sin nombre"}</span>?
+            <Text variant="body" color="#16305A">¿Está seguro de que desea eliminar el asset{" "}
+              <span className="font-semibold">{asset.name || "Sin nombre"}</span>?</Text>
           </p>
-          <p className="text-gray-600 text-sm mt-2">
-            Esta acción no se puede deshacer.
+          <p className="text-gray-600 text-sm my-2">
+            <Text variant="label" color="#16305A">Esta acción no se puede deshacer.</Text>
           </p>
+          <Divider className="bg-[#A7BDE2]" />
         </ModalBody>
         <ModalFooter>
-          <Button color="default" variant="light" onPress={onCancel}>
-            Cancelar
-          </Button>
-          <Button color="danger" onPress={onConfirm}>
-            Eliminar
-          </Button>
+          <div className="flex gap-3 justify-end">
+            <ButtonCitricaAdmin
+              variant="modalv2"
+              className="w-[162px]"
+              onPress={onCancel}
+            >
+              Cancelar
+            </ButtonCitricaAdmin>
+            <ButtonCitricaAdmin
+              variant="primary"
+              className="bg-[#F04242] w-[162px] !border-0"
+              onPress={onConfirm}
+            >
+              Eliminar
+            </ButtonCitricaAdmin>
+          </div>
         </ModalFooter>
       </ModalContent>
     </Modal>
