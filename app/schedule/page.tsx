6452df64a0832540872ae64866a6a585b917a9b1
rokback <rokbackend@gmail.com>
2026-03-20 16:44:31 -0500

    se agregó seccion de schedule

---
+++ app/schedule/page.tsx
@@ -0,0 +1,12 @@
+import { ContactSectionLanding } from "@/shared/components/project-components/contact-section";
+import { FooterCitrica } from "@/shared/components/project-components/footer-citrica";
+
+export default function SchedulePage() {
+  return (
+    <>
+      <ContactSectionLanding variant="home" layout="landing" />
+
+      <FooterCitrica />
+    </>
+  );
+}
