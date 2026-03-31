92a9962c992820b7a151c03099fa1ab5b20f57ad
rokback <rokbackend@gmail.com>
2026-03-30 21:44:15 -0500

    paginas servicio y servicios contratados

--- app/admin/gestion-servicios/servicios-contratados/page.tsx
+++ app/admin/gestion-servicios/servicios-contratados/page.tsx
@@ -2,13 +2,17 @@
 import { Text, Col, Container } from "citrica-ui-toolkit";
 
 export default function ServiciosContratadosPage() {
-
   return (
     <Container>
-      <Col cols={{ lg: 12, md: 6, sm: 4 }}>
-        <h1 className="text-2xl font-bold mb-4">
-          <Text color="#678CC5" variant="title" weight="bold">Gestión de Servicios</Text> {'>'} <Text color="#265197" variant="title" weight="bold">Servicios Contratados</Text>
-        </h1>
+      <Col noPadding cols={{ lg: 12, md: 6, sm: 4 }}>
+        <Text as="h3" variant="title" weight="bold">
+          <Text isAdmin={true} color="#678CC5" variant="title" weight="bold">
+            Gestión de Servicios
+          </Text>
+          <Text isAdmin={true} color="#265197" variant="title" weight="bold">
+            <span> {">"} </span> Servicios Contratados
+          </Text>
+        </Text>
       </Col>
     </Container>
   );
